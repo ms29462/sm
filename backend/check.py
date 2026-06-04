c99704b5ccbdf4dd9f4e3240851ff793f5770425
SM <syllamohamadou@gmail.com>
2026-06-04 08:17:37 -0400

    Remove leftover fix scripts

--- backend/check.py
+++
@@ -1,4 +0,0 @@
-with open(r"C:\Users\Lenovo\sm\backend\server.py", "r", encoding="utf-8") as f:
-    content = f.read()
-idx = content.find('@api_router.get("/my-chats")')
-print(repr(content[idx:idx+600]))
