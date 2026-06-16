4841691dd250166f5a6cc9af68f7225b39cd9ba7
SM <syllamohamadou@gmail.com>
2026-06-15 22:53:25 -0400

    Remove leftover scripts

--- backend/check.py
+++
@@ -1,6 +0,0 @@
-with open(r"C:\Users\Lenovo\sm\frontend\src\lib\api.js", "r", encoding="utf-8") as f:
-    content = f.read()
-import re
-matches = re.findall(r"getClub\w+.*", content)
-for m in matches[:5]:
-    print(m)
