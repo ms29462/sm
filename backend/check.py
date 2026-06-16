7104aeed4f5f9191c9ae42ca4d868caf7b635396
SM <syllamohamadou@gmail.com>
2026-06-15 21:41:45 -0400

    Remove leftover scripts

--- backend/check.py
+++
@@ -1,6 +0,0 @@
-with open(r"C:\Users\Lenovo\sm\frontend\src\components\club\ClubOpportunities.js", "r", encoding="utf-8") as f:
-    lines = f.readlines()
-print(repr(lines[168]))
-print(repr(lines[169]))
-print(repr(lines[170]))
-print(repr(lines[171]))
--- backend/fix_players2.py
+++
@@ -1,8 +0,0 @@
-with open(r"C:\Users\Lenovo\sm\frontend\src\components\club\ClubPlayers.js", "r", encoding="utf-8") as f:
-    lines = f.readlines()
-
-new_lines = [line for line in lines if "<ApprovedGate" not in line and "</ApprovedGate>" not in line]
-
-with open(r"C:\Users\Lenovo\sm\frontend\src\components\club\ClubPlayers.js", "w", encoding="utf-8") as f:
-    f.writelines(new_lines)
-print("Done! Removed", len(lines) - len(new_lines), "lines")
