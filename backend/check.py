2ec708b24d09a88f151743fe9430ab8819283c99
SM <syllamohamadou@gmail.com>
2026-06-18 17:29:45 -0400

    Remove leftover scripts

--- backend/check.py
+++
@@ -1,4 +0,0 @@
-with open(r"C:\Users\Lenovo\sm\frontend\src\components\player\PremiumUpgrade.js", "r", encoding="utf-8") as f:
-    content = f.read()
-idx = content.find("/year")
-print(repr(content[idx-100:idx+20]))
