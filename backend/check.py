95354f7013095890019eab400e4281f65331acd9
SM <syllamohamadou@gmail.com>
2026-06-17 21:51:05 -0400

    Remove leftover scripts

--- backend/check.py
+++
@@ -1,4 +0,0 @@
-with open(r"C:\Users\Lenovo\sm\backend\server.py", "r", encoding="utf-8") as f:
-    content = f.read()
-idx = content.find('"/player/credits/claim-reward"')
-print(repr(content[idx:idx+400]))
